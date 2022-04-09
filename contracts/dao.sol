1d9d2bc7b60f18760090ed481ceda09c62889169
falilah <leogoldf21@gmail.com>
2022-04-09 12:43:14 +0100

    contract logic

--- contracts/dao.sol
+++ contracts/dao.sol
@@ -1,84 +1,112 @@
 pragma solidity ^0.8.0;
 
-contract Governance{
-    
-}
-
-
-contract WhiteList{
-
+contract WhiteList {
     address[] public DAOmembers;
-    uint constant public MINIMUMMEMBER = 5;
-    //struct holding information of business 
-    struct BusinessOwner{
-        string  name;
+    uint256 public constant MINIMUMMEMBER = 2;
+    uint256 public contractBalance;
+    //struct holding information of business
+    struct BusinessOwner {
+        string name;
         address business;
-        uint duration;
-        uint amount;
-        uint AmountGenerated;
+        uint256 amount;
+        uint256 AmountGenerated;
         address[] investors;
-        mapping (address => uint) investorsBalances;
+        mapping(address => uint256) investorsBalances;
     }
-    // members have to  vote before a business can be whiteListed, if vote >= 70% of then an address is whitliested 
-
 
     mapping(address => bool) whiteListedBusiness;
-    mapping (address => uint) VotesCount;
+    mapping(address => mapping(address => bool)) memberVote;
+    mapping(address => uint256) VotesCount;
     mapping(address => BusinessOwner) public businessowner;
 
-    constructor(address[] memory _DAOmembers){
+    constructor(address[] memory _DAOmembers) {
         require(_DAOmembers.length >= MINIMUMMEMBER);
         DAOmembers = _DAOmembers;
-
     }
 
     event Business(address, bool);
 
-    function VoteWhitelisBusiness(address _addr) internal returns(uint){
-        require(checkMemeber(), "not a member of the DAO");
+    // members have to  vote before a business can be whiteListed, if vote >= 70% of then an address is whitliested
+
+    function VoteWhitelistBusiness(address _addr) public returns (uint256) {
+        assert(checkMemeber());
+        assert(!(memberVote[msg.sender][_addr]));
         VotesCount[_addr]++;
-        if(VotesCount[_addr]>=5){
-            // WhiteListBusiness(_addr, amount);
+        if (VotesCount[_addr] >= calMinimumVote()) {
+            WhiteListBusiness(_addr);
         }
-
-
+        memberVote[msg.sender][_addr] = true;
+        return VotesCount[_addr];
     }
-    function checkMemeber() internal view returns(bool){
-        for (uint i; i < DAOmembers.length; i++){
-            if (DAOmembers[i] == msg.sender) return true;
+
+    function VoteBlacklistBusiness(address _addr) public returns (uint256) {
+        assert(checkMemeber());
+        assert((memberVote[msg.sender][_addr]));
+        VotesCount[_addr]++;
+        if (VotesCount[_addr] >= calMinimumVote()) {
+            BlacklistBusiness(_addr);
         }
-        return false;
-
-    }
-    function WhiteListBusiness(address _addr, uint amount) public returns (bool){
-        assert(!whiteListedBusiness[_addr]);
-        businessowner[_addr].amount = amount;
-        whiteListedBusiness[_addr] = true;
-        emit Business(_addr, true);
-    }
-    function BlacklistBusiness(address _addr) public {
-        whiteListedBusiness[_addr] = true;
+        memberVote[msg.sender][_addr] = false;
+        return VotesCount[_addr];
     }
 
-    function viewWhiteListedBusiness(address _addr) public view returns (bool){
-        return whiteListedBusiness[_addr];
+    function AddMembertoDAO(address _addr) public returns (uint256) {
+        assert(checkMemeber());
+        assert(!(memberVote[msg.sender][_addr]));
+        VotesCount[_addr]++;
+        if (VotesCount[_addr] == calMinimumVote()) {
+            DAOmembers.push(_addr);
+        }
+        memberVote[msg.sender][_addr] = true;
+        return VotesCount[_addr];
     }
 
-    function addBusiness(string memory _name, uint duration, address businessAddress) public{
+    // ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"]
+
+    function addBusiness(string memory _name, address businessAddress) public {
         BusinessOwner storage BO = businessowner[businessAddress];
         assert(whiteListedBusiness[businessAddress]);
         BO.business = businessAddress;
         BO.name = _name;
-        BO.duration = duration;       
     }
+
     function InvestInBusiness(address business) public payable {
         //Thinking their money invested on behalf of the business should go straight to supperfluid taggged to a particular business.
         BusinessOwner storage BO = businessowner[business];
         require(msg.value > 0, "you can't invest 0 value");
-        require(BO.amount + msg.value > BO.AmountGenerated, "Required Loan amount met");
+        require(
+            BO.amount + msg.value > BO.AmountGenerated,
+            "Required Loan amount met"
+        );
         BO.AmountGenerated += msg.value;
         BO.investors.push(msg.sender);
         BO.investorsBalances[msg.sender] += msg.value;
+        contractBalance += msg.value;
     }
 
+    function viewWhiteListedBusiness(address _addr) public view returns (bool) {
+        return whiteListedBusiness[_addr];
+    }
+
+    function WhiteListBusiness(address _addr) internal {
+        assert(!whiteListedBusiness[_addr]);
+        // businessowner[_addr].amount = amount;
+        whiteListedBusiness[_addr] = true;
+        emit Business(_addr, true);
+    }
+
+    function BlacklistBusiness(address _addr) internal {
+        whiteListedBusiness[_addr] = false;
+    }
+
+    function checkMemeber() internal view returns (bool status) {
+        status;
+        for (uint256 i; i < DAOmembers.length; i++) {
+            if (DAOmembers[i] == msg.sender) status = true;
+        }
+    }
+
+    function calMinimumVote() internal view returns (uint256 value) {
+        value = (DAOmembers.length * 70) / 100;
+    }
 }
