b245429a151ff8b1a5a005e1f86998aeea99d6a3
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-04-09 18:33:12 +0100

    Pages created

--- client/dao/src/App.js
+++ client/dao/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import Home from "./home/Home";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Business from "./pages/business/Business";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
+  // a flag for keeping track of whether or not a user is connected
+  const [connected, setConnected] = useState(false);
   return (
-    <div className="">
-      <Home />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/business" element={<Business />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/dao/src/components/dashview/DashView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./DashView.module.css";
+
+const DashView = () => {
+  return <div className={Styles.root}>DashView</div>;
+};
+
+export default DashView;
