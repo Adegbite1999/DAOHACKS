448d5dd130f2d10d2950600eb145409577849e3d
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2022-04-10 13:28:44 +0100

    changes

--- client/dao/src/components/layout/Layout.jsx
+++ client/dao/src/components/layout/Layout.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "../navbar/Navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children }) => {
+  const [connected, setConnected] = useState(false);
+
+  const connectWallet = async () => {
+    if (!!window.ethereum || !!window.web3) {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      toast.success('wallet connected')
+      setConnected(true)
+    } else {
+      toast.error("please use an etherum enabled browser")
+      setConnected(false)
+    }
+  };
+
   return (
     <div>
-      <Navbar />
+      <ToastContainer/>
+      <Navbar  
+      connected={connected}
+      connectWallet={connectWallet}
+      />
       {children}
     </div>
   );
