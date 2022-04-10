6f231c632f10c4c08fd6d47cb82278782a01c6d4
Fafemi Adeola <adeola5678@gmail.com>
2022-04-10 01:07:33 +0100

    added vote and info page designs

--- client/dao/src/App.js
+++ client/dao/src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Business from "./pages/business/Business";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Info from "./pages/info/Info";
+import Vote from "./pages/vote/Vote";
 
 function App() {
   // a flag for keeping track of whether or not a user is connected
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/business" element={<Business />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/vote" element={<Vote />} />
       </Routes>
     </div>
   );
